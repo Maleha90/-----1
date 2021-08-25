6f79574088ce8332b64eae6f3dab54421598f38c
Dmitrii <maleha90@mail.ru>
2021-08-25 12:57:50 +0900

    first commit

---
+++ js/click.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+const imgPost = document.querySelector('.img__works')
+const imgOrigin = document.querySelector('.works__img')
+
+imgOrigin.addEventListener('onmouseout', function(event) {
+    this.classList.add('imgPost')
+})
